39f8384df76f23be32fccbf431e66f31a7c22e14
Owain Williams <owain.williams.213@gmail.com>
2023-07-23 20:32:27 +0100

    basic functionality

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['jest-extended/all'],
+  testRegex: '/tests/.*(\\.|/)(test|spec)\\.[jt]sx?$',
+}
