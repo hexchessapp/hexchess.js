8368f0161ce53045ec27ee9e2af54e5db30ed410
Owain Williams <owain.williams.213@gmail.com>
2023-07-28 16:09:53 +0100

    const sharing issue

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { HexChess, PAWN, WHITE } from './src/hexchess'
-
-const fen = '6/7/8/9/10/k4K5/10/9/8/7/6 w - 0 1'
-const chess = new HexChess(fen)
-chess.put('g5', { color: WHITE, type: PAWN })
-console.log(chess.inCheck())
