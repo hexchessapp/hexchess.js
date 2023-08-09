4baa8408b26145167e835f28d46e61537fa168d2
Owain Williams <owain.williams.213@gmail.com>
2023-08-09 16:09:25 +0100

    adding tested endgames

---
+++ __tests__/isDraw.test.ts
@@ -0,0 +1,39 @@
+import { HexChess } from '../src/hexchess'
+
+test('isDraw checkmate', () => {
+  const fen = '6/7/8/9/10/K7Q1k/10/9/8/7/6 b - 0 1'
+  const chess = new HexChess(fen)
+  expect(chess.isDraw()).toEqual(false)
+})
+
+test('isDraw insuffient', () => {
+  const fen = '6/7/8/9/10/K9k/10/9/8/7/6 w - 0 1'
+  const chess = new HexChess(fen)
+  expect(chess.isDraw()).toEqual(true)
+})
+
+test('isDraw stalemate', () => {
+  const fen = '6/7/8/7P1/10/K9k/10/7Q1/8/7/6 b - 0 1'
+  const chess = new HexChess(fen)
+  expect(chess.isDraw()).toEqual(false)
+})
+
+test('isDraw threefold', () => {
+  const chess = new HexChess()
+  chess.move('f1', 'g2'); chess.move('f11', 'g9'); chess.move('g2', 'f1'); chess.move('g9', 'f11')
+  chess.move('f1', 'g2'); chess.move('f11', 'g9'); chess.move('g2', 'f1'); chess.move('g9', 'f11')
+  chess.move('f1', 'g2')
+  expect(chess.isDraw()).toEqual(true)
+})
+
+test('isDraw 50-move', () => {
+  const fen = '6/7/8/9/10/KP8k/10/9/8/7/6 b - 100 50'
+  const chess = new HexChess(fen)
+  expect(chess.isDraw()).toEqual(true)
+})
+
+test('isDraw 50-move-almost', () => {
+  const fen = '6/7/8/9/10/KP8k/10/9/8/7/6 b - 99 50'
+  const chess = new HexChess(fen)
+  expect(chess.isDraw()).toEqual(false)
+})
--- src/hexchess.ts
+++ src/hexchess.ts
@@ -271,6 +271,13 @@ export function getHexColor(hex: Hexagon): BoardColor {
   return color
 }
 
+function arrayEquals(a: Array<unknown>, b: Array<unknown>) {
+  return Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index]);
+}
+
 export class HexChess {
   private _board: Record<Hexagon, Piece | null> = emptyBoard()
   private _turn: Color = WHITE
@@ -520,16 +527,41 @@ export class HexChess {
     return this.isAttacked(king) && this.moves().length == 0
   }
 
-  isDraw() {
-    return
+  isDraw(): boolean {
+    return this.isInsufficientMaterial() || this.isThreefoldRepetition() || this._halfMoves >= 100
   }
 
-  isInsufficientMaterial() {
-    return
+  isInsufficientMaterial(): boolean {
+    const piecesPerColor: Record<Color, Array<PieceSymbol>> = {w:[], b:[]}
+    HEXAGONS.forEach((hex) => {
+      const piece = this._board[hex]
+      if (!piece) {
+        return
+      }
+      piecesPerColor[piece.color].push(piece.type)
+    });
+    if (
+      piecesPerColor.w.length > 2 && piecesPerColor.w.length > 2 || 
+      piecesPerColor.b.length > 2 && piecesPerColor.b.length
+    ) {
+      return false
+    }
+    const whiteSorted = piecesPerColor.w.sort()
+    const blackSorted = piecesPerColor.b.sort()
+    if (
+      arrayEquals(whiteSorted, ['k']) && arrayEquals(blackSorted, ['k']) ||
+      arrayEquals(whiteSorted, ['k']) && arrayEquals(blackSorted, ['b', 'k']) ||
+      arrayEquals(whiteSorted, ['k']) && arrayEquals(blackSorted, ['k', 'n']) ||
+      arrayEquals(whiteSorted, ['b', 'k']) && arrayEquals(blackSorted, ['k']) ||
+      arrayEquals(whiteSorted, ['k', 'n']) && arrayEquals(blackSorted, ['k'])
+      ) {
+      return true
+    }
+    return false
   }
 
-  isGameOver() {
-    return
+  isGameOver(): boolean {
+    return this.isDraw() || this.isCheckmate() || this.isStalemate() || this.isThreefoldRepetition()
   }
 
   isStalemate(this: HexChess): boolean {
@@ -538,24 +570,28 @@ export class HexChess {
       return false
     }
 
-    let moves = 0
-
-    for (let i = 0; i < HEXAGONS.length; i++) {
-      const piece = this.get(HEXAGONS[i])
-      if (piece == null) {
-        continue
-      }
-      if (piece.color != this._turn) {
-        continue
-      }
-      moves += this._possibleMoves(HEXAGONS[i]).length
-    }
-
-    return !this.isAttacked(king) && moves == 0
+    return !this.isAttacked(king) && this.moves().length == 0
   }
 
-  isThreefoldRepetition() {
-    return
+  isThreefoldRepetition(): boolean {
+    const allStates = this._history.map((move) => {
+      return move.after.split(" ").slice(0, 2).join(" ")
+    })
+    const occurances = new Map<string, number>([])
+    allStates.forEach(state => {
+      const value = occurances.get(state)
+      if (!value) {
+        return occurances.set(state, 1)
+      }
+      occurances.set(state, value + 1)
+    });
+    let isRepetition = false
+    occurances.forEach((value) => {
+      if (value > 2) {
+        isRepetition = true
+      }
+    })
+    return isRepetition
   }
 
   load(fen: string): Error | null {
